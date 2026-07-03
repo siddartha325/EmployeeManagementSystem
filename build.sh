eb404f145fe51ebbd8fadd628c9246bb136d16dc
Siddartha Ravuri <siddartharavuri@gmail.com>
2026-07-03 18:47:02 +0530

    Create admin on deployment

--- build.sh
+++ build.sh
@@ -6,3 +6,14 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 python manage.py migrate
+
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser(
+        "admin",
+        "admin@gmail.com",
+        "admin123"
+    )
+EOF
